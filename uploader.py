c8ff6d1db3e51e52faaaf8135f8b9bc7ac55489d
Andrew Li <andrewli2403@gmail.com>
2021-12-29 19:36:37 -0800

    uploader successful using pickle

--- uploader.py
+++ uploader.py
@@ -1,8 +1,9 @@
 from oauth2client.service_account import ServiceAccountCredentials
 import gspread
-
+import gspread_dataframe as gd
 import pandas as pd
-
+from df2gspread import df2gspread as d2g
+import pickle
 
 scope = ['https://www.googleapis.com/auth/spreadsheets', "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
 
@@ -10,8 +11,23 @@ creds = ServiceAccountCredentials.from_json_keyfile_name('cal-basketball-data-sc
 
 client = gspread.authorize(creds)
 
+spreadsheet_key = '16G1DxZy2-X5wLcqjWRRWxHaZo0bcXl2TOHFukrhMEhY'
+
+#grab DataFrame from data_collection
+with open('pickledata.p', 'rb') as fh:   #you need to use 'rb' to read
+    df = pickle.load(fh)
+
 #.worksheet("SHEET NAME")
 sheet = client.open("Cal Post Game Analytics").sheet1
+wks_name = 'Sheet1'
 
-sheet.update_cell(2, 4, "asdf")
+#flips sheets into pandas DataFrame to append data_collection grab
+existing = gd.get_as_dataframe(sheet)
+existing = pd.DataFrame(sheet.get_all_records())
+existing.drop([],axis=1,inplace=True)
+updated = existing.append(df)
+gd.set_with_dataframe(sheet, updated)
+
+
+d2g.upload(df, spreadsheet_key, wks_name, credentials=creds, row_names=True)
 
